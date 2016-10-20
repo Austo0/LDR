2f937e312e4178936eab04a6a4fefbd6b9220819
Austen <austenfogarty@gmail.com>
2016-10-21 09:48:02 +1300

    done

--- LDR.c
+++ LDR.c
@@ -12,25 +12,29 @@
 #include "lcd.h"
 #include <util/delay.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define debounce_time  50
 
 static volatile int count;
-static timer_count;	// Debounce time delay in hundredths on milliseconds
-static debounce_count;
-
-static volatile int square1;	// Count of PHA
-static volatile int square2;	// Count of PHB
+static int timer_count;	// Debounce time delay in hundredths on milliseconds
+static int debounce_count;
 
 static uint8_t previous_pin;
 static uint8_t pin;
 
+static float time_count = 0;
+static float rotation_count = 0;
+
+static volatile float rpm;			// RPM of motor
+static volatile int direction;		// motor direction
+
  // Initialise the timer and pin change interrupts
  void Initialise_Interrupts()
  {
-			/* PHA on Port B pin 0 --- route via PCINT0 */
 			/* PHB on Port B pin 1 --- route via PCINT1 */
+	 	 	/* PHA on Port B pin 0 --- route via PCINT0 */
 	 	 	/* LDR Comparator on Port B pin 4 --- route via PCINT4 */
 	        PCMSK0 |= (1 << PCINT0) | (1 << PCINT1) | (1 << PCINT4) ;
 	        PCICR  |= (1 << PCIE0);
@@ -41,6 +45,18 @@ static uint8_t pin;
 	        TCCR0B |= (1 << CS02) | (1 << CS00) ;	// 1024 pre-scaler
 	        TIMSK0 |= (1 << OCIE0A);				// Enable interrupt flag when timer is matched with OCR0A
 
+	        /* Initialise timer1 interrupts */
+	        TCCR1B |= (1 << WGM12);	                // Set the timer to CTC mode
+	        OCR1A = 2000;							// Value to count to
+	        TCCR1B |= (1 << CS11) ;					// 8 pre-scaler
+	        TIMSK1 |= (1 << OCIE1A);				// Enable interrupt flag when timer is matched with OCR0A
+
+	        /* Initialise timer2 interrupts */
+	        OCR2A = 0;								// Initial value to count to
+	        TCCR2A |= (1 << COM2A1);				// Set non-inverting mode
+	        TCCR2A |= (1 << WGM20) | (1 << WGM21);	// Set fast PWM mode
+	        TCCR2B |= (1 << CS20) ;					// No pre-scaling
+
 	        /* Globally enable interrupts */
 	        sei();
  }
@@ -48,25 +64,39 @@ static uint8_t pin;
 int main(void)
 {
 	Initialise_Interrupts();	//Initialise interrupts
-	/* Set up Port B; input 1 on Bit 0 */
-	DDRB = 0;
+	/* Set up Port B; input 1 on Bit 0, PWM out of bit 3*/
+	DDRB |= (1 << PB3) ;
 	/* No pull-up resistor on inputs */
 	PORTB = 0x00;
 	lcd_init('c');		// Initialise the LCD
-	char str[20];
+	char str[33];
+	char rpm_read [10];
+	char duty[10];
 
 	while (1)
 	{
+		cli();							// Disable interrupts
+		dtostrf(rpm,2,0,rpm_read);
+		dtostrf(((float)OCR2A/255)*100,2,0,duty);	// Calculate duty cycle
+		sei();							// Enable interrupts
+
 		//Concatenate to an array
-		snprintf(str,20,"%i  %i\n%i",square1,square2,count);
+		snprintf(str,33,"RPM = %s    %i\nDuty = %s%c   %i ",rpm_read,count,duty,'%',OCR2A);
 		lcd_display(str,0);		// Display the time
-		_delay_ms(50);			// Short delay
+		_delay_ms(500);			// Short delay
     	lcd_clear();			//Clear the LCD
 	}
 	return 0;
 }
 
+
 // Timer interrupt
+ISR(TIMER1_COMPA_vect)
+{
+time_count++;	// increment every 1 ms
+}
+
+// Timer0 interrupt for debounce
 ISR(TIMER0_COMPA_vect)
 {
 	if (timer_count == debounce_time) // Set number of timer interrupts until count can be updated again
@@ -87,12 +117,29 @@ ISR(PCINT0_vect)
 	// If the interrupt is on the rising edge of PHA
 	if((pin & (1<<PB0)) && !(previous_pin & (1<<PB0)))
 	{
-		square1++;
+		if(rotation_count < 276)
+		{
+			rotation_count++;
+		}
+		else
+		{
+			rpm = (1/(float)time_count)*60*1000;	// Calculate the RPM
+			time_count = 0;
+			rotation_count = 0;
+		}
 	}
 	// If the interrupt is on the rising edge of PHB
 	if((pin & (1<<PB1)) && !(previous_pin & (1<<PB1)))
 	{
-		square2++;
+		// Determine direction of motor
+		if(pin & (1<<PB0))
+		{
+			direction = 1;	// Forward direction
+		}
+		else
+		{
+			direction = -1;	// Reverse direction
+		}
 	}
 
 	previous_pin = pin;	// Save current pin state for the next interrupt
@@ -104,6 +151,14 @@ ISR(PCINT0_vect)
 			{
 				count++;
 				debounce_count++;
+				if(OCR2A >= 255)
+				{
+					OCR2A = 0;		// Reset duty cycle
+				}
+				else
+				{
+					OCR2A += 10;	// Increment PWM duty
+				}
 			}
 	}
 }
